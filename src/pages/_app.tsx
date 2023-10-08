9400aab6b95da08d9e4f9a710f16fa2b3ddf2f8f
franciscomaik <chiconunes3081@gmail.com>
2023-10-08 20:40:59 -0300

    feat: config project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import theme from '../styles/theme';
 
 const MyApp: React.FC<AppProps> = function ({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
